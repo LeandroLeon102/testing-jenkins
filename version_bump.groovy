61364f1f460bd9c5f0d0236cafeadef6416e9fb5
LeandroLeon102 <51277661+LeandroLeon102@users.noreply.github.com>
2023-08-23 15:48:16 -0600

    chore: update jenkins pipeline jenkinsfile

--- version_bump.groovy
+++ version_bump.groovy
@@ -1,10 +1,10 @@
 pipeline {
     agent any
 
-
     stages {
         stage('checkout repository') {
             steps {
+                cleanWs()
                 checkout([
                     $class: 'GitSCM',
                     branches: [[
@@ -18,14 +18,40 @@ pipeline {
             }
         }
 
-
         stage('run gitversion') {
             steps {
                 dir('repo') {
-                    sh 'pwd; ls'
-                    sh 'gitversion'
+                    sh 'gitversion -output buildserver'
+                    script {
+                        def props = readProperties file: 'gitversion.properties'
+                        env.GitVersion_SemVer = props.GitVersion_SemVer
+                        env.GitVersion_BranchName = props.GitVersion_BranchName
+                        env.GitVersion_AssemblySemVer = props.GitVersion_AssemblySemVer
+                        env.GitVersion_MajorMinorPatch = props.GitVersion_MajorMinorPatch
+                        env.GitVersion_Sha = props.GitVersion_Sha
+                    }
                 }
+            }
+        }
 
+        stage('version bump') {
+            steps {
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[
+                        name: '*/main'
+                    ]],
+                    extensions: [cloneOption(honorRefspec: true, noTags: false, reference: 'repo', shallow: false), localBranch('main'), pruneStaleBranch(), [$class: 'RelativeTargetDirectory', relativeTargetDir: 'repo']],
+                    userRemoteConfigs: [[
+                        url: 'https://github.com/LeandroLeon102/testing-jenkins'
+                    ]]
+                ])
+                dir('repo') {
+                    sh """  
+                        git tag "${GitVersion_MajorMinorPatch}" -m "release version ${GitVersion_MajorMinorPatch}"
+                        git push origin --tags
+                    """
+                }
             }
         }
     }
