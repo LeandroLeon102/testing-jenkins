0f0f65258a263aed53c349eeec6f47f9d7f47864
LeandroLeon102 <51277661+LeandroLeon102@users.noreply.github.com>
2023-08-30 13:31:57 -0600

    chore(pipeline definition): Update pipeline definition

--- pipeline_definition.groovy
+++ pipeline_definition.groovy
@@ -1,9 +1,9 @@
-pipelineJob ('misc/version_bump') {
-    description('Version Bump for Azure DevOps repository')
+pipelineJob ('') {
+    description('')
 
     parameters {[
-        stringParam('BRANCH', 'feature/version-bump', 'Target branch Name'),
-        stringParam('REPOSITORY', 'DevSecOps/isec-terraform-modules', 'Target repository name')
+        stringParam('BRANCH', '', 'Target branch Name'),
+        stringParam('REPOSITORY', '', 'Target repository name')
     ]}
 
     properties {
@@ -19,14 +19,14 @@ pipelineJob ('misc/version_bump') {
     definition {
         cpsScm {
             lightweight(false)
-            scriptPath('jenkinsfiles/misc/version-bump.groovy')
+            scriptPath('')
             scm {
                 git {
                     browser()
                     branch('${BRANCH}')
                     remote {
-                        credentials('/jenkins/default/azure-devops/repo/isec-jenkins-dsl/ssh-creds')
-                        url('indiepay@vs-ssh.visualstudio.com:v3/indiepay/DevSecOps/isec-jenkins-dsl')
+                        credentials('/')
+                        url('')
                     }
                 }
             }
