ec9e9c1be17c9f4c5486ca27e60cdea561b53fad
LeandroLeon102 <51277661+LeandroLeon102@users.noreply.github.com>
2023-08-30 13:28:36 -0600

    chore(jenkinsfile): updated commitizen jenkinsfile

--- commitizen_jenkins_file.groovy
+++ commitizen_jenkins_file.groovy
@@ -1,45 +1,35 @@
-pipeline {
-    agent any
+pipelineJob ('') {
+    description('')
 
-    stages {
-        stage('checkout repository') {
-            steps {
-                cleanWs()
-                checkout([
-                    $class: 'GitSCM',
-                    branches: [[
-                        name: '*/main'
-                    ]],
-                    extensions: [cloneOption(honorRefspec: true, noTags: false, reference: 'repo', shallow: false), localBranch('main'), pruneStaleBranch(), [$class: 'RelativeTargetDirectory', relativeTargetDir: 'repo']],
-                    userRemoteConfigs: [[
-                        url: 'https://github.com/LeandroLeon102/testing-jenkins'
-                    ]]
-                ])
-            }
-        }
-        
-        stage('version bump'){
-            steps {
-                dir('repo') {
-                    sh 'git log --format=oneline'
-                    sh 'git tag'
-                    script {
-                        def status = sh(returnStatus: true, script: "cz bump")
-                        if (status == 21) {
-                            message """
-                                Last Commit message has no valid action for version bump. Forcing minor version bump...
-                            """
-                            sh 'git commit --allow-empty -m "fix: force patch version bump - no valid action in last commit message"'
-                            sh 'cz bump'
-                        }
-                    }
-                    sh 'git log --format=oneline'
-                    sh 'git tag'
-                    sh 'git remote set-url origin git@github.com:LeandroLeon102/testing-jenkins ; git config --global --add safe.directory /var/jenkins_home/workspace/commitizen-test/repo'
-                    sh 'git push --set-upstream origin main'
-                    sh 'git push origin --tags'
+    parameters {[
+        stringParam('BRANCH', '', 'Target branch Name'),
+        stringParam('REPOSITORY', '', 'Target repository name')
+    ]}
+
+    properties {
+        pipelineTriggers {
+            triggers {
+                pollSCM { 
+                    scmpoll_spec('*/2 * * * *')
                 }
             }
         }
     }
-}
+
+    definition {
+        cpsScm {
+            lightweight(false)
+            scriptPath('')
+            scm {
+                git {
+                    browser()
+                    branch('${BRANCH}')
+                    remote {
+                        credentials('')
+                        url('')
+                    }
+                }
+            }
+        }
+    }
+} 
