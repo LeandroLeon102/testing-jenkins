4625014be462601aa093c93f26e476c297a2ffb9
LeandroLeon102 <51277661+LeandroLeon102@users.noreply.github.com>
2023-08-30 13:31:03 -0600

    chore(jenkinsfile): updated jenkinsfile

--- commitizen_jenkins_file.groovy
+++ commitizen_jenkins_file.groovy
@@ -1,35 +1,50 @@
-pipelineJob ('') {
-    description('')
+pipeline {
+    agent {label ""}
+    options {buildDiscarder(logRotator(daysToKeepStr: "7", numToKeepStr: "10"))}
 
-    parameters {[
-        stringParam('BRANCH', '', 'Target branch Name'),
-        stringParam('REPOSITORY', '', 'Target repository name')
-    ]}
-
-    properties {
-        pipelineTriggers {
-            triggers {
-                pollSCM { 
-                    scmpoll_spec('*/2 * * * *')
-                }
+    stages{
+        stage('Checkout repository'){
+            steps {
+                checkout scm: [
+                    $class: 'GitSCM',
+                    branches: [[name: params.BRANCH]],
+                    extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: params.REPOSITORY]],
+                    userRemoteConfigs: [[
+                        credentialsId: '',
+                        url: "/${REPOSITORY}"
+                    ]]
+                ]
             }
         }
-    }
 
-    definition {
-        cpsScm {
-            lightweight(false)
-            scriptPath('')
-            scm {
-                git {
-                    browser()
-                    branch('${BRANCH}')
-                    remote {
-                        credentials('')
-                        url('')
+        stage('version bump'){
+            steps {
+                dir(params.REPOSITORY) {
+                    script {
+                        def status = sh(returnStatus: true, script: "cz bump")
+                        if (status == 21) {
+                            sh """
+                                echo 'Last Commit message has no valid action for version bump. Forcing minor version bump...'
+                            """
+                            sh 'git commit --allow-empty -m "fix: force patch version bump - no valid action in last commit message"'
+                            sh 'cz bump'
+                        }
                     }
                 }
             }
         }
     }
-} 
+    
+    post {
+        success {
+            dir(params.REPOSITORY) {
+                sshagent (credentials: [']) {
+                    sh("git checkout ${BRANCH}")
+                    sh("git push origin")
+                    sh('git push origin --tags')
+                }
+            }
+        }
+    }
+}
+  
